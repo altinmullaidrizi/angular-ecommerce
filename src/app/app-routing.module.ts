651d3794e2865cf1a750c323790103ee8d171aad
Altini <altin.m@vitamina-d.agency>
2021-07-16 10:15:39 +0200

    Created components & views

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomepageComponent} from './views/homepage/homepage.component';
+import {ContactComponent} from './views/contact/contact.component';
+import {AboutComponent} from './views/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { HomepageComponent } from './views/homepage/homepage.component';
+import { AboutComponent } from './views/about/about.component';
+import { ContactComponent } from './views/contact/contact.component';
+import { StatisticsComponent } from './components/statistics/statistics.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProductsListComponent,
+    HomepageComponent,
+    AboutComponent,
+    ContactComponent,
+    StatisticsComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
